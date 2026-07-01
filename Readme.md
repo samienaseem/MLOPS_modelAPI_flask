5ee7aadda2f7490f34babe1c9f0882397be53dda
Saquib Naseem <31252335+samienaseem@users.noreply.github.com>
2026-07-01 01:47:05 +0100

    Flask api added

---
+++ Readme.md
@@ -0,0 +1,1 @@
+Flask applcation demo
---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask,request
+
+import pickle
+
+
+app=Flask(__name__)
+
+@app.route("/ping",methods=["GET"])
+def ping():
+    return {"Message": "Hello from the server, yes i am active"}
+
+
+# loading the model 
+model_pickle=open('./artifacts/classifier1.pkl',"rb")
+clf=pickle.load(model_pickle)
+
+
+# /predict endpoint
+@app.route("/predict", methods=['POST'])
+def predict():
+   loan_req = request.get_json()
+   print(loan_req)
+   
+   
+   if loan_req['Gender'] == "Male":
+        Gender = 0
+   else:
+        Gender = 1
+   if loan_req['Married'] == "Unmarried":
+        Married = 0
+   else:
+        Married = 1
+ 
+   if loan_req['Credit_History'] == "Unclear Debts":
+        Credit_History = 0
+   else:
+        Credit_History = 1  
+    
+   ApplicantIncome = loan_req['ApplicantIncome']
+   LoanAmount = loan_req['LoanAmount']
+
+   result=clf.predict([Gender,Married,Credit_History,ApplicantIncome,LoanAmount])
+
+   if result==0:
+        pred="Rejected"
+   else:
+        pred="Approved"
+
+   return {"loan_application_status": pred}
+   
+    
